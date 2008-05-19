e3e950f25370b7e8e69b5ae3b7a786e846629ad6
kiorky <devnull@localhost>
2008-05-19 11:26:54 +0200

    0.4 changes

--- hooks/proj.py
+++ hooks/proj.py
@@ -3,7 +3,9 @@ import os,sys
 
 def installmaps(options,buildout):
     # adding maps files
-    os.system('cd nad')
-    os.system('tar xzvf %s/maps/proj-datumgrid-1.3.tar.gz'% buildout['buildout']['directory'])
-    os.system('cd ..')
+    os.system("""
+cd %s/nad
+tar xzvf %s/maps/proj-datumgrid-1.3.tar.gz
+cd .. """ %( options['compile-directory'], buildout['buildout']['directory']))
+
 
